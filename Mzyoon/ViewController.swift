7448a2d699f74f191f536eb785c955056a1a9812
QOL <qol@qols-mac-mini.local>
2018-10-16 10:19:24 +0530

    Initial Commit

---
+++ Mzyoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mzyoon
+//
+//  Created by QOL on 16/10/18.
+//  Copyright © 2018 QOL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
